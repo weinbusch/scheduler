987a847f6becce8101fb3be5cb6f2901d14d62dc
Martin Bierbaum <>
2022-07-20 08:13:14 +0200

    split tests into modules

---
+++ solver/tests/test_models.py
@@ -0,0 +1,102 @@
+import datetime
+
+from django.db import IntegrityError
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from solver.models import DayPreference, Schedule
+
+User = get_user_model()
+
+
+class ModelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username="foo", password="1234")
+
+    def test_user_creation_also_creates_user_preferences(self):
+        self.assertIsNotNone(self.user.user_preferences)
+
+    def test_day_preference_available(self):
+        p = DayPreference.objects.create(
+            user_preferences=self.user.user_preferences,
+            start=datetime.date.today(),
+        )
+        self.assertTrue(p.available)
+
+    def test_day_preferences_unique_date(self):
+        p = self.user.user_preferences
+        s = datetime.date(2022, 7, 11)
+        DayPreference.objects.create(user_preferences=p, start=s)
+        with self.assertRaises(IntegrityError):
+            DayPreference.objects.create(user_preferences=p, start=s)
+
+    def test_available_dates(self):
+        p = self.user.user_preferences
+        d = datetime.date(2022, 7, 3)
+        days = [d + datetime.timedelta(days=delta) for delta in [0, 2, 4]]
+        DayPreference.objects.bulk_create(
+            [DayPreference(user_preferences=p, start=day) for day in days]
+        )
+        self.assertListEqual(
+            p.get_available_dates(
+                start=datetime.date(2022, 7, 4), end=datetime.date(2022, 7, 6)
+            ),
+            [datetime.date(2022, 7, 5)],
+        )
+
+    def test_schedule_start_end_date(self):
+        Schedule.objects.create(
+            start=datetime.date(2022, 7, 12),
+            end=datetime.date(2022, 8, 12),
+        )
+
+    def test_schedule_users_many_to_many(self):
+        u1 = self.user
+        u2 = User.objects.create_user(username="bar", password="1234")
+        Schedule.objects.create(
+            start=datetime.date(2022, 7, 12),
+            end=datetime.date(2022, 8, 12),
+        ).users.set([u1, u2])
+        Schedule.objects.create(
+            start=datetime.date(2022, 7, 12), end=datetime.date(2022, 8, 12)
+        ).users.set([u1])
+        self.assertEqual(u1.schedules.count(), 2)
+        self.assertEqual(u2.schedules.count(), 1)
+
+    def test_schedule_days(self):
+        s = Schedule(
+            start=datetime.date(2022, 7, 11),  # a monday
+            end=datetime.date(2022, 7, 18),  # the next monday
+        )
+        self.assertListEqual(
+            s.days(),
+            [datetime.date(2022, 7, x) for x in [11, 12, 13, 14, 15, 18]],
+        )
+
+    def test_schedule_solve(self):
+        u1 = User.objects.create(username="bar", password="1234")
+        u2 = User.objects.create(username="baz", password="1234")
+        user_sequence = [u1, u1, u2, u2, u1, u2]
+        s = Schedule.objects.create(
+            start=datetime.date(2022, 7, 11),  # a monday
+            end=datetime.date(2022, 7, 18),  # the next monday
+        )
+        days = s.days()
+        s.users.set([u1, u2])
+        DayPreference.objects.bulk_create(
+            [
+                DayPreference(user_preferences=u.user_preferences, start=s)
+                for u, s in zip(user_sequence, days)
+            ]
+        )
+        self.assertListEqual(
+            [
+                {"start": d, "title": u.username}
+                for d, u in zip(
+                    days,
+                    user_sequence,
+                )
+            ],
+            s.solve(),
+        )
---
+++ solver/tests/test_solver.py
@@ -0,0 +1,91 @@
+import math
+import unittest
+import datetime
+import collections
+
+from solver.solver import get_schedule
+
+
+class SchedulerTest(unittest.TestCase):
+    def test_each_day_is_assigned_one_family(self):
+        """Test a simple case with two families and four days"""
+        days = [
+            datetime.date.today(),
+            datetime.date.today() + datetime.timedelta(days=1),
+            datetime.date.today() + datetime.timedelta(days=2),
+            datetime.date.today() + datetime.timedelta(days=3),
+        ]
+        families = ["foo", "bar"]
+        available_dates = {f: days for f in families}
+        schedule = get_schedule(days, families, available_dates)
+        self.assertTrue(all([f in families for _, f in schedule]))
+
+    def test_equal_number_of_assignements(self):
+        """Test each family is assigned an equal number of days"""
+        n = 100
+        families = ["foo", "bar", "baz", "bak"]
+        days = [
+            datetime.date.today() + datetime.timedelta(days=x)
+            for x in range(len(families) * n)
+        ]
+        available_dates = {f: days for f in families}
+        schedule = get_schedule(days, families, available_dates)
+        counts = collections.Counter([f for _, f in schedule])
+        self.assertTrue(all([counts[f] == n for f in families]))
+
+    def test_uneven_number_of_days(self):
+        """Test with 2 families and 3 days"""
+        days = [datetime.date.today() + datetime.timedelta(days=x) for x in range(3)]
+        families = ["foo", "bar"]
+        available_dates = {f: days for f in families}
+        schedule = get_schedule(days, families, available_dates)
+        counts = collections.Counter([f for _, f in schedule])
+        self.assertTrue(
+            (counts["foo"] == 2 and counts["bar"] == 1)
+            or (counts["foo"] == 1 and counts["bar"] == 2)
+        )
+
+    def test_no_family_more_than_two_less_than_maximum(self):
+        """If the number of days is not dividable by the number of families,
+        no family should have less than floor(days/families) number of
+        assignments
+
+        """
+        for n in range(9, 12):
+            with self.subTest(n=n):
+                families = ["foo", "bar", "baz"]
+                days = [
+                    datetime.date.today() + datetime.timedelta(days=x) for x in range(n)
+                ]
+                available_dates = {f: days for f in families}
+                schedule = get_schedule(days, families, available_dates)
+                counts = collections.Counter([f for _, f in schedule])
+                self.assertTrue(
+                    all([counts[f] >= math.floor(n / len(families)) for f in families])
+                )
+
+    def test_with_constraints(self):
+        """First family is only available on the first and last day"""
+        days = [
+            datetime.date.today(),
+            datetime.date.today() + datetime.timedelta(days=1),
+            datetime.date.today() + datetime.timedelta(days=2),
+            datetime.date.today() + datetime.timedelta(days=3),
+        ]
+        families = ["foo", "bar"]
+        available_dates = {"foo": [days[0], days[3]], "bar": days}
+        schedule = get_schedule(days, families, available_dates)
+        self.assertListEqual([f for _, f in schedule], ["foo", "bar", "bar", "foo"])
+
+    def test_infeasible_constraints(self):
+        """First family is only available on a single day"""
+        days = [
+            datetime.date.today(),
+            datetime.date.today() + datetime.timedelta(days=1),
+            datetime.date.today() + datetime.timedelta(days=2),
+            datetime.date.today() + datetime.timedelta(days=3),
+        ]
+        families = ["foo", "bar"]
+        available_dates = {"foo": [days[0]], "bar": days}
+        with self.assertRaises(Exception):
+            get_schedule(days, families, available_dates)
