fba8228f34232a469f50446294e3bda4abde5c81
Martin Bierbaum <>
2022-08-01 22:42:21 +0200

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((pyvenv-activate . "~/projects/python/scheduler/venv")
+		 (elpy-test-runner . elpy-test-django-runner))))
