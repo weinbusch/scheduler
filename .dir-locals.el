240191a0627020cbe28f32c7ccd20975f1c1e9b2
Martin Bierbaum <example@example.com>
2022-08-13 11:50:18 +0200

    fix: remove pyvenv-activate from dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((pyvenv-activate . "~/projects/python/scheduler/venv")
-		 (elpy-test-runner . elpy-test-django-runner))))
+((python-mode . ((elpy-test-runner . elpy-test-django-runner))))
