a9ca2719f65339ae65a06b8c6ba1f2f53df4e04d
Martin Bierbaum <example@example.com>
2022-09-08 09:08:19 +0200

    frontend: add api.js module

---
+++ solver/static/solver/js/api.js
@@ -0,0 +1,52 @@
+function API(days_url, preferences_url, assignments_url, csrf_token){
+    this.addDay = function(dateStr){
+        return fetch(days_url, {
+            method: "PATCH",
+            headers: {
+                "X-CSRFToken": csrf_token,
+            },
+            body: JSON.stringify({
+                date: dateStr,
+            }),
+        })
+    }
+
+    this.removeDay = function(dateStr){
+        return fetch(days_url, {
+            method: "DELETE",
+            headers: {
+                "X-CSRFToken": csrf_token,
+            },
+            body: JSON.stringify({
+                date: dateStr,
+            }),
+        })
+    }
+
+    this.addPreference = function(name, dateStr){
+        return fetch(preferences_url, {
+            method: "PATCH",
+            headers: {
+                "X-CSRFToken": csrf_token,
+            },
+            body: JSON.stringify({
+                name: name,
+                date: dateStr,
+            }),
+        })    
+    }
+
+    this.deletePreference = function(name, dateStr){
+        return fetch(preferences_url, {
+            method: "DELETE",
+            headers: {
+                "X-CSRFToken": csrf_token,
+            },
+            body: JSON.stringify({
+                name: name,
+                date: dateStr,
+            }),
+        })    
+    }
+}
+
