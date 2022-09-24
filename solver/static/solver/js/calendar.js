d85ff0aa9ee3f20fd288ac8b7cc4cee73c6fcc78
Martin Bierbaum <example@example.com>
2022-09-24 22:57:22 +0200

    refactor: move part of participant calendar code into calendar.js

--- solver/static/solver/js/calendar.js
+++ solver/static/solver/js/calendar.js
@@ -14,3 +14,35 @@ function Calendar(selector, options){
         dateClick: options.dateClick ? _.debounce(options.dateClick, 200) : undefined,
     })
 }
+
+function ParticipantCalendar(selector, options){
+    let getSelectedParticipant = options.getSelectedParticipant;
+    options.eventClick = function(info){
+        let participant = info.event.extendedProps.participant,
+            dateStr = info.event.startStr;
+        if (participant && participant == getSelectedParticipant()){
+            api.removePreference(participant, dateStr).then((r) => {if (r.ok){
+                this.refetchEvents()
+            }})
+        }
+    };
+    options.dateClick = function(info){
+        let participant = getSelectedParticipant(),
+            dateStr = info.dateStr,
+            events = this.getEvents(),
+            isAvailable = events.filter(e => 
+                e.startStr == dateStr && e.source.id == "days"
+            ).length == 1,
+            isEmpty = events.filter(e => {
+                e.startStr == dateStr && e.extendedProps.participant == participant
+            }).length == 0;
+        if (isAvailable && isEmpty){
+            api.addPreference(participant, dateStr).then(r => {
+                if (r.ok){
+                    this.refetchEvents()
+                }
+            })
+        }
+    };
+    return Calendar(selector, options);
+}
