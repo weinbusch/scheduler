6f585c5a750f410a2a5d498d48d9b069be93a0e4
Martin Bierbaum <example@example.com>
2022-09-24 23:18:25 +0200

    refactor: urls.py

--- solver/urls.py
+++ solver/urls.py
@@ -9,7 +9,11 @@ auth_patterns = [
 ]
 
 api_patterns = [
-    path("schedules/<int:pk>", views.schedule_api, name="schedule"),
+    path(
+        "schedules/<int:pk>",
+        views.schedule_api,
+        name="schedule",
+    ),
     path(
         "schedules/<int:pk>/days",
         views.schedule_days_api,
@@ -27,34 +31,38 @@ api_patterns = [
     ),
 ]
 
-urlpatterns = [
-    path("", views.schedule_list, name="index"),
-    path("schedules/add", views.add_schedule, name="add_schedule"),
+schedule_patterns = [
+    path("add", views.add_schedule, name="add_schedule"),
     path(
-        "schedules/<int:pk>/settings",
+        "<int:pk>/settings",
         views.schedule_settings,
         name="schedule_settings",
     ),
     path(
-        "schedules/<int:pk>/preferences",
+        "<int:pk>/preferences",
         views.schedule_preferences,
         name="schedule_preferences",
     ),
     path(
-        "schedules/<int:pk>/assignments",
+        "<int:pk>/assignments",
         views.schedule_assignments,
         name="schedule_assignments",
     ),
     path(
-        "schedules/<int:pk>/delete",
+        "<int:pk>/delete",
         views.delete_schedule,
         name="delete_schedule",
     ),
     path(
-        "schedules/<int:pk>/participants/delete",
+        "<int:pk>/participants/delete",
         views.delete_participant,
         name="delete_participant",
     ),
+]
+
+urlpatterns = [
+    path("", views.schedule_list, name="index"),
+    path("schedules/", include(schedule_patterns)),
     path("auth/", include((auth_patterns, "solver"), namespace="auth")),
     path("api/", include((api_patterns, "solver"), namespace="api")),
 ]
