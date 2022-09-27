0a8b4714c8eda74fa7dec720ebe1735090579ef1
Martin Bierbaum <example@example.com>
2022-09-27 20:40:50 +0200

    refactor: refactor solver.py and tests

--- solver/solver.py
+++ solver/solver.py
@@ -1,112 +1,73 @@
 import datetime
-import math
-import scipy
+import scipy.optimize
 
 
-def get_schedule(days, available_dates, window=None):
-    """get_schedule(days, available_dates)
+def get_schedule(days, preferences, window=0):
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.milp.html#scipy.optimize.milp
 
-    Parameters:
+    resources = set(preferences.keys())
+    labels = [(d, r) for r in resources for d in days]
 
-        days: list of datetime.date objects indicating all dates to
-              which a family has to be assigned to
+    def binary_vector(f):
+        return [1 if f(r, d) else 0 for r in resources for d in days]
 
-        available_dates: dictionary of family name/date list pairs
-                         indicating on which dates each family is
-                         available. If a family is not included in the
-                         dictionary, its date list is considered
-                         empty. If a given date is not included in a
-                         family's date list, that family is *not*
-                         available on this date and *cannot* be
-                         assigned to this date. This might lead to an
-                         infeasible model, if too many dates are
-                         unavailable
+    # The coefficients of the linear objective function to be minimized
+    c = binary_vector(lambda r, d: True)
 
-    Returns:
+    # The type of integrality constraint on each decision variable.
+    integrality = c
 
-        schedule: list of datetime.date/family name pairs
-
-    Note: The optimizer works under the following constraints:
-
-    1. each date must be assigned to exactly one family.
-
-    2. the maximum number of dates assigned to any given family must
-       not exceed math.ceil(len(days)/len(families)) and must not be
-       lower than math.floor(len(days)/len(families)).
-
-    3. a family cannot be assigned to a date that is not listed in its
-       available dates.
-
-    """
-    families = list(available_dates.keys())
-    constraints = make_constraints(days, families, available_dates)
-    result = run_model(len(days) * len(families), constraints)
-    return make_schedule(result, days, families)
-
-
-def run_model(n, constraints):
-    c = [1 for _ in range(n)]
-    r = scipy.optimize.milp(
-        c=c,
-        integrality=c,
-        constraints=constraints,
-        bounds=(0, 1),
-    )
-    if not r.success:
-        raise Exception(r.message)
-    return r.x
-
-
-def make_schedule(a, days, families):
-    m = a.reshape(len(days), len(families))
-    return [
-        (days[i], families[j])
-        for i, row in enumerate(m)
-        for j, x in enumerate(row)
-        if x == 1
+    # Constraints
+    constraints = [
+        one_resource_per_day(binary_vector, days),
+        equal_assignments(binary_vector, resources, days),
+        respect_preferences(binary_vector, preferences),
+        # consecutive_assignments(binary_vector, resources, days, window),
     ]
 
+    # solution
+    res = scipy.optimize.milp(c, integrality=integrality, constraints=constraints)
 
-def make_constraints(days, families, available_dates):
-    constraints = []
+    if res.x is None:
+        raise Exception(res.message)
 
-    # 1. For each day there must be exactly 1 assigned family
+    solution = sorted(label for label, x in zip(labels, res.x) if x == 1)
+    return solution
 
-    for day in days:
-        constraint = scipy.optimize.LinearConstraint(
-            [1 if d == day else 0 for d in days for _ in families], 1, 1
-        )
-        constraints.append(constraint)
 
-    # 2. No family should do more than days/families and less than
-    # (days/families)-1 of assignments
+def one_resource_per_day(f, days):
+    # Exactly one resource per day
+    return ([f(lambda r, d: d == day) for day in days], 1, 1)
 
-    max_assignments = math.ceil(len(days) / len(families))
 
-    for family in families:
-        constraint = scipy.optimize.LinearConstraint(
-            [1 if f == family else 0 for _ in days for f in families],
-            max_assignments - 1,
-            max_assignments,
-        )
-        constraints.append(constraint)
+def equal_assignments(f, resources, days):
+    # Not more than ceil(days/resources) and not less than
+    # floor(days/resources) assignments per resouce
+    x = len(days) // len(resources)
+    return ([f(lambda r, d: r == resource) for resource in resources], x, x + 1)
 
-    # 3. Availability
 
-    for family in families:
-        constraint = scipy.optimize.LinearConstraint(
-            [
-                1 if f == family and day not in available_dates.get(family, []) else 0
-                for day in days
-                for f in families
-            ],
-            0,
-            0,
-        )
-        constraints.append(constraint)
+def respect_preferences(f, preferences):
+    # No assignments if day is not in preferences
+    return (
+        [
+            f(lambda r, d: r == resource and d not in available)
+            for resource, available in preferences.items()
+        ],
+        0,
+        0,
+    )
 
-    """TODO:
-    - a family should not be assigned more than once every n days
-    """
 
-    return constraints
+def consecutive_assignments(f, resources, days, window):
+    # Not more than 1 assigment per resource on n consecutive days
+    delta = datetime.timedelta(days=window)
+    return (
+        [
+            f(lambda r, d: r == resource and day >= d and day - d < delta)
+            for resource in resources
+            for day in days
+        ],
+        0,
+        1,
+    )
--- solver/tests/test_solver.py
+++ solver/tests/test_solver.py
@@ -1,46 +1,48 @@
 import pytest
 import datetime
 from collections import Counter
+from itertools import groupby
 
 from solver.solver import get_schedule
 
+days = {datetime.date(2022, 1, d) for d in range(1, 31)}
 
-def test_equal_number_of_assignments():
-    days = [datetime.date(2022, 1, d) for d in range(1, 31)]
+
+def test_problem_can_be_solved_by_an_equal_number_of_assignments():
     preferences = {name: days for name in "abc"}
     assignments = get_schedule(days, preferences)
     counter = Counter(name for day, name in assignments)
     assert counter == {name: 10 for name in preferences}
 
 
-def test_unequal_number_of_assignments():
-    days = [datetime.date(2022, 1, d) for d in range(1, 31)]
+def test_problem_requires_an_unequal_number_of_assignments():
     preferences = {name: days for name in "abcd"}
     assignments = get_schedule(days, preferences)
     counter = Counter(name for day, name in assignments)
-    assert counter == {name: 8 if name < "c" else 7 for name in preferences}
+    assert all(7 <= x <= 8 for x in counter.values())
 
 
 def test_respect_preferences():
-    days = [datetime.date(2022, 1, d) for d in range(1, 31)]
     preferences = {
-        "a": [day for day in days if day.day % 2 == 0],
-        "b": [day for day in days if day.day % 2 == 1],
+        "a": {day for day in days if day.day % 2 == 0},
+        "b": {day for day in days if day.day % 2 == 1},
     }
     assignments = get_schedule(days, preferences)
-    assert [day for day, name in assignments if name == "a"] == preferences["a"]
-    assert [day for day, name in assignments if name == "b"] == preferences["b"]
+    assert {day for day, name in assignments if name == "a"} == preferences["a"]
+    assert {day for day, name in assignments if name == "b"} == preferences["b"]
 
 
-def test_consecutive_assignments():
-    days = [datetime.date(2022, 1, d) for d in range(1, 31)]
+def test_infeasible_preferences():
+    available = days.copy()
+    available.pop()
+    preferences = {name: available for name in "ab"}
+    with pytest.raises(Exception, match="infeasible"):
+        get_schedule(days, preferences)
+
+
+def test_solution_contains_consecutive_assignments():
     preferences = {name: days for name in "ab"}
     assignments = get_schedule(days, preferences)
-    assert assignments == [(day, "a" if day.day < 16 else "b") for day in days]
-
-
-def test_infeasible_constraints():
-    days = [datetime.date(2022, 1, d) for d in range(1, 31)]
-    preferences = {name: days[1:-1] for name in "ab"}
-    with pytest.raises(Exception):
-        get_schedule(days, preferences, window=2)
+    assert any(
+        len(list(g)) > 1 for k, g in groupby(name for day, name in assignments)
+    ), "No consecutive elements"
